7c8238f3d2da2314fb1834008f1fc9a04e478e99
kevinsultana <kevinpdg.ks@gmail.com>
2025-04-16 20:42:48 +0700

    feat CTA Whatsapp

--- src/components/CTAWhastapp.jsx
+++ src/components/CTAWhastapp.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function CTAWhastapp() {
   return (
-    <div>
+    <button onClick={() => window.open("https://wa.me/628129924049")}>
       <img
         src="/whatsappicon.png"
         alt="Whatsapp"
         className="fixed bottom-2 right-2 w-12 md:w-16 md:bottom-4 md:right-4"
       />
-    </div>
+    </button>
   );
 }
